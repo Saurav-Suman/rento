c6dd0bddc676ce8f651acb1c8a5716441ed43a64
Saurav <jnu.saurav@gmail.com>
2017-05-21 15:21:16 +0800

    Create sync.js

---
+++ sync.js
@@ -0,0 +1,45 @@
+var request = require('request');
+var cheerio = require('cheerio');
+function scrapePage(arg, callback) {
+    request(arg, function(err, response, body){
+        if(body){
+            var $ = cheerio.load(body);
+            var hyperLinks = $('a');
+            for(var i=0; i<hyperLinks.length;i++){
+                console.log(items);
+                if($(hyperLinks[i]).attr('href')){
+                    items.push($(hyperLinks[i]).attr('href'));
+                }
+            }
+            setTimeout(function() {
+                launcher();
+                callback(null);
+            }, 1000);
+        }
+    });
+   
+}
+function final() { console.log('Done', results); }
+
+var items = [ 'https://reddit.com'];
+var results = [];
+var running = 0;
+var limit = 1;
+
+function launcher() {
+    while(running < limit && items.length > 0) {
+        var item = items.shift();
+        scrapePage(item, function(result) {
+            results.push(item);
+            running--;
+            if(items.length > 0) {
+                launcher();
+            } else if(running == 0) {
+                final();
+            }
+        });
+        running++;
+    }
+}
+
+launcher();
